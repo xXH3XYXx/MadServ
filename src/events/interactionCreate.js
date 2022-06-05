4a5fcfa9dc2ed40e6fde7a7983a0b82195146606
xXH3XYXx <91407775+xXH3XYXx@users.noreply.github.com>
2022-06-05 00:45:00 -0500

    Revert "Update to include commands from the TinyGnome bot - updates to bot.js, ready.js, interactionCreate.js and messageCreate.js to accommodate difference in code prior to first merge (#1)"
    This reverts commit 86f2138180ebe2106bee8b05a9c88dd65901f976.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,58 +1,21 @@
 module.exports = {
 	name: "interactionCreate",
 	async execute(interaction) {
-		if (!interaction.isCommand()) {
-			return;
-		}
-		if (interaction.client.commands.has(interaction.commandName)) {
-			switch (interaction.commandName) {
-				case 'tgcounter':
-					if (interaction.member.permissions.has([Permissions.FLAGS.MODERATE_MEMBERS])) {
-						if (interaction.options.getSubcommand() === 'register') {
-							CounterInteractions.registertgcounter(interaction);
-						}
-						else if (interaction.options.getSubcommand() === 'update') {
-							CounterInteractions.updatetgcounter(interaction);
-						}
-					}
-					else {
-						interaction.reply('You do not have permission to use this command!');
-						return;
-					}
-					break;
-				case 'tgcountdown':
-					if (interaction.member.permissions.has([Permissions.FLAGS.SEND_MESSAGES])) {
-						if (interaction.options.getSubcommand() === 'add') {
-							CountdownInteractions.add(interaction);
-						}
-						else if (interaction.options.getSubcommand() === 'delete') {
-							CountdownInteractions.delete(interaction);
-						}
-					}
-					else {
-						interaction.reply('You do not have permission to use this command!');
-						return;
-					}
-					break;
-				default:
-					const command = interaction.client.commands.get(interaction.commandName);
+		if (!interaction.isCommand()) return;
 
-					if (!command) {
-						return;
-					}
+		const command = interaction.client.commands.get(interaction.commandName);
 
-					try {
-						await command.execute(interaction);
-					} catch (err) {
-						if (err) console.error(err);
+		if (!command) return;
 
-						await interaction.reply({
-							content: "An error occurred while executing that command.",
-							ephemeral: true,
-						});
-					}
-					break;
-			}
+		try {
+			await command.execute(interaction);
+		} catch (err) {
+			if (err) console.error(err);
+
+			await interaction.reply({
+				content: "An error occurred while executing that command.",
+				ephemeral: true,
+			});
 		}
 	}
 }
--- src/models/countdownSettings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const CountdownSettingsSchema = new mongoose.Schema({
-  guild_id: String,
-  user_id: String,
-  name: String,
-  date: String
-});
-
-module.exports = mongoose.model("CountdownSettings", CountdownSettingsSchema);
