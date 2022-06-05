ff48b7572007256d409afe4205290392785857df
Kayla Barnett <schultz.kayla.l@gmail.com>
2022-06-04 21:28:38 -0500

    Update to include commands from the TinyGnome bot - updates to bot.js, ready.js, interactionCreate.js and messageCreate.js to accommodate difference in code prior to first merge

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,21 +1,58 @@
 module.exports = {
 	name: "interactionCreate",
 	async execute(interaction) {
-		if (!interaction.isCommand()) return;
+		if (!interaction.isCommand()) {
+			return;
+		}
+		if (interaction.client.commands.has(interaction.commandName)) {
+			switch (interaction.commandName) {
+				case 'tgcounter':
+					if (interaction.member.permissions.has([Permissions.FLAGS.MODERATE_MEMBERS])) {
+						if (interaction.options.getSubcommand() === 'register') {
+							CounterInteractions.registertgcounter(interaction);
+						}
+						else if (interaction.options.getSubcommand() === 'update') {
+							CounterInteractions.updatetgcounter(interaction);
+						}
+					}
+					else {
+						interaction.reply('You do not have permission to use this command!');
+						return;
+					}
+					break;
+				case 'tgcountdown':
+					if (interaction.member.permissions.has([Permissions.FLAGS.SEND_MESSAGES])) {
+						if (interaction.options.getSubcommand() === 'add') {
+							CountdownInteractions.add(interaction);
+						}
+						else if (interaction.options.getSubcommand() === 'delete') {
+							CountdownInteractions.delete(interaction);
+						}
+					}
+					else {
+						interaction.reply('You do not have permission to use this command!');
+						return;
+					}
+					break;
+				default:
+					const command = interaction.client.commands.get(interaction.commandName);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+					if (!command) {
+						return;
+					}
 
-		if (!command) return;
+					try {
+						await command.execute(interaction);
+					} catch (err) {
+						if (err) console.error(err);
 
-		try {
-			await command.execute(interaction);
-		} catch (err) {
-			if (err) console.error(err);
-
-			await interaction.reply({
-				content: "An error occurred while executing that command.",
-				ephemeral: true,
-			});
+						await interaction.reply({
+							content: "An error occurred while executing that command.",
+							ephemeral: true,
+						});
+					}
+					break;
+			}
 		}
 	}
 }
